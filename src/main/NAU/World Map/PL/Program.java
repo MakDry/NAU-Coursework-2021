94441faa9e07234c9b379b7a17e06ae10da8d98c
MakDry <fakecounter050@gmail.com>
2021-12-16 04:15:09 +0200

    PL: basic structure of methods was formed

--- src/main/NAU/World Map/PL/Program.java
+++ src/main/NAU/World Map/PL/Program.java
@@ -1,9 +1,13 @@
 package PL;
 
+import BLL.World;
+
 public class Program {
     public static void main(String[] args) {
+        MenuView view = new MenuView();
+        World world = new World();
 
-        Menu menu = new Menu();
+        Menu menu = new Menu(world, view);
         menu.menuController();
     }
 }
