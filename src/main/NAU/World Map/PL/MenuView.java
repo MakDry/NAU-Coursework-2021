48ec09fe6bc9338524095f8d413cfae49898cfd0
MakDry <fakecounter050@gmail.com>
2021-12-16 04:13:27 +0200

    The basic structure of tests was formed

---
+++ src/main/NAU/World Map/PL/MenuView.java
@@ -0,0 +1,2 @@
+package PL;public class MenuView {
+}
---
+++ src/test/Test World/testWorld.java
@@ -0,0 +1,52 @@
+import BLL.World;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class testWorld {
+    private World world;
+
+    @BeforeEach
+    void testSetUp(){
+        world = new World();
+    }
+
+    @Test
+    void testChangeCityParameters(){
+
+    }
+
+    @Test
+    void testChangeCountryParameters(){
+
+    }
+
+    @Test
+    void testSearchCityInCountry(){
+
+    }
+
+    @Test
+    void testSearchCountry(){
+
+    }
+
+    @Test
+    void testRemoveCountry(){
+
+    }
+
+    @Test
+    void testAddCountry(){
+
+    }
+
+    @Test
+    void testRemoveCityFromCountry(){
+
+    }
+
+    @Test
+    void testAddCityToCountry(){
+
+    }
+}
